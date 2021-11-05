de21efd971bae853267609ec6c01cdfbfa406b14
manoharvirati <40772700+manoharvirati@users.noreply.github.com>
2021-11-05 12:22:48 +0530

    Create kit.py

---
+++ kit.py
@@ -0,0 +1,14 @@
+import pywhatkit as kit
+import time
+#kit.image_to_ascii_art(r"joker.jpeg",r"text_image.txt")
+
+#kit.sendwhatmsg("+91{7989599936}","hello",11,59)
+#time.sleep(30)
+
+#kit.playonyt("neffex-cold")
+
+#kit.search("bitcoin")
+
+#kit.info("gold")
+
+kit.text_to_handwriting("Welcom to this channel",rgb=(0,0,138))
